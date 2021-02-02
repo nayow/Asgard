6dafa57c12cb90e054688317f46cd97e6d300b06
nayow <nayow@hotmail.fr>
2021-02-02 21:00:52 +0100

    new packages

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import "./assets/main.scss";
 import { gsap } from "gsap";
+import VueCookies from "vue-cookies";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
 createApp(App)
-  .use(router, gsap)
+  .use(router, gsap, VueCookies)
   .mount("#app");
